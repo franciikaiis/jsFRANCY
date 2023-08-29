cc383fcca37e30fe7ab4ebb1796cf2b586ee58a4
franciikaiis <142934899+franciikaiis@users.noreply.github.com>
2023-08-29 16:07:25 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,18 @@
+ion setup() {
+    createCanvas(600, 600);
+    background("white") ; 
+  }
+  
+  function draw() {
+    
+    
+    stroke("blue");
+    fill("red");
+    
+    //console.log(mouseIsPressed);
+    
+    if (mouseIsPressed) {
+    rect(mouseX, mouseY, 20, 35);
+    }
+        
+  }
